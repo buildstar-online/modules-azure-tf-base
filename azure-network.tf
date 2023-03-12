6d925eb991a96f176bec3c9fdb68ac19e08d96be
Max! <admin@cloudydev.net>
2023-03-12 00:01:41 -0800

    Update azure-network.tf

--- azure-network.tf
+++ azure-network.tf
@@ -13,19 +13,4 @@ resource "azurerm_network_security_group" "netsec_group" {
   tags = {
     environment = var.environment
   }
-
-}
-
-resource "azurerm_network_security_rule" "ssh" {
-  name                        = "inboundSSH"
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "Tcp"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefixes      = var.allowed_ips
-  destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.resource_group.name
-  network_security_group_name = azurerm_network_security_group.netsec_group.name
 }
