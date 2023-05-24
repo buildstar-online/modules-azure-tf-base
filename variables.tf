8555ef8f309ce99845a43d0d31cd0cb5f4cee79f
Max! <admin@cloudydev.net>
2023-05-24 20:26:38 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "vnet_address_space" {
 
 variable "allowed_ips" {
   description = "addresses allowed to access the infra"
-  type        = list(string)
+  type        = string
 }
 
 variable "cr_sku" {
