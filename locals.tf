7cd307747414293c42136ef25fe2ea4a19405a4f
Max! <admin@cloudydev.net>
2023-05-24 21:50:18 +0200

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
   generated_users = tolist(["${azurerm_user_assigned_identity.admin_identity.principal_id}", "${var.runner_object_id}"])
-  all_users       = concat(var.admin_users, local.generated_users)
+  all_users       = concat(["${var.admin_users}"], local.generated_users)
 }
